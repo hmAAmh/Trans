f17fe203baee7a8f5548dd05d183fa7d02977595
hmAAmh <103527265+hmAAmh@users.noreply.github.com>
2022-08-05 22:23:26 -0700

    Menu Implimented

--- Assets/Scripts/button_scr.cs
+++ Assets/Scripts/button_scr.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class button_scr : MonoBehaviour
 {
+    bool clicked = false;
     sceneManager_scr managerScr;
+    public GameObject blackScreen;
     // Start is called before the first frame update
     void Start(){
         managerScr = GameObject.Find("sceneManager").GetComponent<sceneManager_scr>();
     }
 
     // Update is called once per frame
-    void TaskOnClick(){
-        if(managerScr.drawable){
-            
+    public void TaskOnClick(){
+        if(!clicked){
+            Debug.Log("test!");
+            clicked = true;
+            RuntimeManager.CreateInstance("event:/sceneEnd").start();
+            GameObject fadeOut = Instantiate(blackScreen);
+            fadeOut.GetComponent<fadeIn_scr>().fadeIn = false;
+            fadeOut.GetComponent<fadeIn_scr>().active = true;
+            fadeOut.transform.position = new Vector3(0f, 0f, 0f);
         }
     }
 }
